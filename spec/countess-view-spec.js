80b87448951329790b6489870f80425043ebb585
Kyle West <kyle.west@familysearch.org>
2017-10-06 13:12:47 -0600

    Added comments to the major modules

--- spec/countess-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import CountessView from '../lib/countess-view';
-
-describe('CountessView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
